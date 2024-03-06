5911a2605a28dcf22636f6a1931709d91cac8614
Suresh Packiry <sureshpackiry73@gmail.com>
2024-03-06 19:49:17 +1100

    getting stuck taking a break

---
+++ script.js
@@ -0,0 +1,16 @@
+let newBookBtn = document.querySelector(".add-btn");
+newBookBtn.addEventListener("click", function() {
+    let newBookForm = document.querySelector(".popup").computedStyleMap.display = "flex";
+})
+
+
+
+let myLibrary = [];
+
+function Book() {
+
+};
+
+function addBookToLibrary() {
+
+}
