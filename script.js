d51a6b1e57743f71a0e9db81a9a52085d39c6b2e
Suresh Packiry <sureshpackiry73@gmail.com>
2024-03-06 23:14:19 +1100

    starting on the js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array for stored books
+const myLibrary = [];
+
+// Select the add button
+let addBook = document.querySelector("#add");
+
+// Showing the dialog box on button click
+addBook.addEventListener("click", function() {
+    dialog.showModal();
+});
+
+
+
